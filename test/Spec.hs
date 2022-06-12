16a7b962926ab88443b3d9fa4fc8cb8544ab8a67
Jesse Hallett <jesse.hallett@hasura.io>
2022-06-12 10:32:23 -0400

    specs

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,38 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE QuasiQuotes #-}
+
+import Autodocodec (eitherDecodeJSONViaCodec, toJSONViaCodec)
+import Data.Aeson.QQ.Simple (aesonQQ)
+import Data.Either (isLeft)
+import Lib
+import Test.Hspec
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = hspec $ do
+  describe "openApi" $ do
+    it "serializes v1 example" $ do
+      let output = V1 $ ExampleV1 {exampleTextField = Nothing, exampleIntField = 1}
+      let expected = [aesonQQ|{ "version": 1, "text": null, "int": 1 }|]
+      toJSONViaCodec output `shouldBe` expected
+
+    it "serializes v2 example" $ do
+      let output = V2 $ ExampleV2 {exampleV2TextField = "hello", exampleV2IntField = 1, exampleV2BoolField = True}
+      let expected = [aesonQQ|{ "version": 2, "text": "hello", "int": 1, "bool": true }|]
+      toJSONViaCodec output `shouldBe` expected
+
+    it "parses v1 example" $ do
+      let input = "{\"version\": 1, \"text\": null, \"int\": 1 }"
+      let expected = V1 $ ExampleV1 {exampleTextField = Nothing, exampleIntField = 1}
+      let actual = eitherDecodeJSONViaCodec input :: Either String Lib.Example
+      actual `shouldBe` Right expected
+
+    it "parses v2 example" $ do
+      let input = "{\"version\": 2, \"text\": \"hello\", \"int\": 1, \"bool\": true }"
+      let expected = V2 $ ExampleV2 {exampleV2TextField = "hello", exampleV2IntField = 1, exampleV2BoolField = True}
+      let actual = eitherDecodeJSONViaCodec input :: Either String Lib.Example
+      actual `shouldBe` Right expected
+
+    it "fails parsing v1 on version mismatch" $ do
+      let input = "{\"version\": 2, \"text\": null, \"int\": 1 }"
+      let actual = eitherDecodeJSONViaCodec input :: Either String Lib.Example
+      actual `shouldSatisfy` isLeft
